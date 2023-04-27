1ddba3c4ed01a7ee6ded38e041b5a97a28751bee
qromp <34507786+qromp@users.noreply.github.com>
2023-04-27 16:30:37 +0900

    Initial commit

---
+++ src/init.lua
@@ -0,0 +1,12 @@
+return {
+	Janitor = require(script.Parent.Janitor),
+	Net = require(script.Parent.Net),
+	Promise = require(script.Parent.Promise),
+	React = require(script.Parent.React),
+	RoactCompat = require(script.Parent.RoactCompat),
+	RoactRodux = require(script.Parent.RoactRodux),
+	Rodux = require(script.Parent.Rodux),
+	Sift = require(script.Parent.Sift),
+	Signal = require(script.Parent.Signal),
+	t = require(script.Parent.t),
+}
